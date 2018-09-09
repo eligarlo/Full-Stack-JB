c256ebb9dc84c1bbd16e918452d66bea6dab5a29
eligarlo <eligarlolo@gmail.com>
2018-09-09 11:54:48 +0300

    Responsive Added

--- Projects/First Project HTML5/templates.js
+++ Projects/First Project HTML5/templates.js
@@ -3,9 +3,9 @@
 function boardTitleTemplate() {
     return `
         <div class="row">
-            <div class="col-md"></div>
-            <div class="col-md bckimg"></div>
-            <div class="col-md"></div>
+            <div class="col-sm"></div>
+            <div class="col-sm bckimg"></div>
+            <div class="col-sm"></div>
         </div>
     `
 }
@@ -14,40 +14,40 @@ function createTaskTemplate() {
     return `
     <div class="container">
     <div class="row title">
-        <div class="col-md"></div>
-        <div class="col-md-3">
+        <div class="col-sm"></div>
+        <div class="col-sm-3">
             <h2 id="headerNewTask">Task Title:</h2>
         </div>
-        <div class="col-md-7">
+        <div class="col-sm-7">
             <input id="titleTask" type="text" class="inputValidation" placeholder="What are you planing to do?" maxlength="15" style="width:250px" required/>
             <p class="alertNotification"></p>
         </div>
     </div>
 </div>
     <div class="row">
-        <div class="col-md-1 newTask-text-center"></div>
-        <div class="col-md-10 newTask-text-padding">
+        <div class="col-sm-1 newTask-text-center"></div>
+        <div class="col-sm-10 newTask-text-padding">
             <p class="alertNotification"></p>
             <textarea id="textTask" type="text" class="inputValidation" placeholder="Type it here..." rows="3" required></textarea>            
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md"></div>
-            <div class="col-md-3">
+            <div class="col-sm"></div>
+            <div class="col-sm-3">
                 <h3 id="headerNewTask">Goal Time:</h3>
             </div>
-            <div class="col-md"></div>
+            <div class="col-sm"></div>
         </div>
         <div class="row">
-            <div class="col-md"></div>
-            <div class="col-md-5">
+            <div class="col-sm"></div>
+            <div class="col-sm-5">
                 <label><strong>Date:</strong>
                 <input id="dateTask" type="text" class="inputValidation" placeholder="dd/mm/yyyy" maxlength="10" style="width:150px" required/>
                 </label>
                 <p class="alertNotification"></p>
             </div>
-            <div class="col-md-5">
+            <div class="col-sm-5">
                 <label><strong>Time:</strong>
                 <input id="timeTask" class="inputValidation" placeholder="00:00" maxlength="11" style="width:150px" required/>
                 </label>
@@ -55,9 +55,8 @@ function createTaskTemplate() {
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md"></div>
-        <div class="col-md-7">
+    <div  style="width:100%; text-align: center;">
+        <div>
             <button onclick="taskBoard.startSite.createNote()" id="addTaskBtn" class="btn btn-primary">Add Task</button>
         </div>
     </div>
@@ -66,29 +65,26 @@ function createTaskTemplate() {
 
 function displayNoteTemplate() {
     return `
-        <div  id="{{title}}1" class="col-md-2 notes-img note show-remove {{justPosted}}">
-            <div class="row">
-                <div class="col-md"><span class="underline"><strong>{{title}}</strong></span></div>
-                <div class="col-md-4">
+    
+        <div  id="{{title}}1" class="col-sm-2 notes-img note show-remove {{justPosted}}">
+            <div class="row align-items-start">
+                <div class="col-sm"><span class="underline"><strong>{{title}}</strong></span></div>
+                <div class="col-sm">
                     <i onclick="taskBoard.startSite.removeNote(id)" id="{{title}}" class="far fa-trash-alt remove"></i>
                 </div>
             </div>
-            <div class="row">                
-                <div class="col-md-11">
+            <div class="row align-items-center">                
+                <div class="col">
                 <div id="textNote">{{text}}</div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-8">
+            <div class="row align-items-end">
+                <div class="col-8">
                     <span>{{date}}</span>
                 </div>
-                <div class="col-md"></div>
-            </div>
-            <div class="row">                          
-                <div class="col-md-8">
+                <div class="col-8">
                     <span>{{time}}</span>
                 </div>
-                <div class="col-md"></div> 
             </div>            
         </div>   
 
