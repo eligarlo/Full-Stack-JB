1bf933bece922b280cff1716269e6f48717b6672
eligarlo <eligarlolo@gmail.com>
2018-09-16 00:47:11 +0300

    Edit Task added

--- Projects/First Project HTML5/main.js
+++ Projects/First Project HTML5/main.js
@@ -1,4 +1,5 @@
 let taskBoard = (function () {
+
         let startSite = {
 
                 init: function () {
@@ -7,12 +8,11 @@ let taskBoard = (function () {
                         this.cacheDomMain();
                         this.cacheDomFooter();
                         this.renderTemplates();
-                        this.expiredNote();
+                        notes.expiredNote();
                 },
                 cacheDomHeader: function () {
 
                         this.header = document.getElementById('title');
-
                 },
                 cacheDomMain: function () {
 
@@ -27,7 +27,7 @@ let taskBoard = (function () {
                         this.header.innerHTML = boardTitleTemplate();
                         this.main.innerHTML = createTaskTemplate();
                         this.footer.innerHTML = '';
-                        this.displayLocalStorage();
+                        notes.displayLocalStorage();
                         for (let i = 0; i < notes.notesArray.length; i++) {
                                 if (notes.notesArray[i].justPosted === 'invisi') {
                                         this.footer.innerHTML += Mustache.to_html(displayNoteTemplate(), notes.notesArray[i]);
@@ -36,14 +36,74 @@ let taskBoard = (function () {
                                         }, 1);
                                         setTimeout(() => {
                                                 notes.notesArray[i].justPosted = 'visi';
-                                                this.saveLocalStorage();
+                                                notes.saveLocalStorage();
                                         }, 400);
                                 } else if (notes.notesArray[i].justPosted === 'visi') {
                                         this.footer.innerHTML += Mustache.to_html(displayNoteTemplate(), notes.notesArray[i]);
+                                        notes.saveLocalStorage();
                                 }
                         }
                 },
                 createNote: function () {
+
+                        notes.cacheTemplate();
+                        notes.validateContent();
+                },
+                removeNote: function (id) {
+
+                        for (let i = 0; i < notes.notesArray.length; i++) {
+                                if (notes.notesArray[i].title === id) {
+
+                                        notes.notesArray.splice(i, 1);
+                                        document.getElementById(id + 1).setAttribute('style', 'transition:0.4s ease-out; opacity:0;');
+                                        notes.expired[i].setAttribute('style', 'transition:0.4s ease-out; opacity:0;');
+
+                                        setTimeout(() => {
+                                                notes.saveLocalStorage();
+                                                this.renderTemplates();
+                                                notes.expiredNote();
+                                        }, 500);
+                                }
+                        }
+                },
+        }
+
+        let notes = {
+
+                notesArray: [{
+                        title: "Example",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie tortor vitae gravida aliquam. Pellentesque ultrices sapien at felis hendrerit vestibulum. In hac habitasse platea dictumst. Suspendisse sit amet lorem id leo varius sodales. Ut sit amet massa ligula. Mauris sit amet tortor nec tellus tincidunt scelerisque. Aliquam eget mauris vel nunc cursus placerat.",
+                        date: '08/09/2018',
+                        time: '11:11',
+                        justPosted: 'visi'
+                },
+                {
+                        title: "Example2",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie tortor vitae gravida aliquam. Pellentesque ultrices sapien at felis hendrerit vestibulum. In hac habitasse platea dictumst. Suspendisse sit amet lorem id leo varius sodales. Ut sit amet massa ligula. Mauris sit amet tortor nec tellus tincidunt scelerisque. Aliquam eget mauris vel nunc cursus placerat.",
+                        date: '08/11/2018',
+                        time: '11:11',
+                        justPosted: 'visi'
+                },
+                {
+                        title: "Example3",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie tortor vitae gravida aliquam. Pellentesque ultrices sapien at felis hendrerit vestibulum. In hac habitasse platea dictumst. Suspendisse sit amet lorem id leo varius sodales. Ut sit amet massa ligula. Mauris sit amet tortor nec tellus tincidunt scelerisque. Aliquam eget mauris vel nunc cursus placerat.",
+                        date: '08/09/2018',
+                        time: '11:11',
+                        justPosted: 'visi'
+                },
+                ],
+                NoteConstractor: function (title, text, date, time) {
+
+                        this.title = title;
+                        this.text = text;
+                        this.date = date;
+                        this.time = time;
+                        this.justPosted = 'invisi';
+
+                        return notes.notesArray.push({ title: this.title, text: this.text, date: this.date, time: this.time, justPosted: this.justPosted });
+
+                },
+                cacheTemplate: function () {
                         this.inputTitle = document.getElementById('titleTask').value;
                         this.inputTask = document.getElementById('textTask').value;
                         this.inputDate = document.getElementById('dateTask').value;
@@ -52,7 +112,8 @@ let taskBoard = (function () {
                         this.alert = document.getElementsByClassName('alertNotification');
                         this.dateRegex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-\.](0?[1-9]|1[012])[\/\-\\.]\d{4}$/g;
                         this.timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/g;
-                        //--------Start of Cache Date and Time Input for validation--------//
+
+                                                        //--------Start of Cache Date and Time Input for validation--------//
                         this.day = this.inputDate.split('/')[0];
                         this.month = this.inputDate.split('/')[1];
                         this.year = this.inputDate.split('/')[2];
@@ -61,8 +122,7 @@ let taskBoard = (function () {
                         this.d = new Date(this.year, (this.month - 1), this.day, (new Date().getHours()), (new Date().getMinutes()), (new Date().getSeconds()), (new Date().getMilliseconds()));
                         this.h = new Date(this.year, (this.month - 1), this.day, this.hour, this.minute);
                         this.currentDate = new Date();
-                        //--------END Of Cache Date and Time Input for validation--------//
-                        this.validateContent();
+                                                        //--------END Of Cache Date and Time Input for validation--------//
                 },
                 validateContent: function () {
                         //--------Start of all inputs are fill up--------//
@@ -111,10 +171,6 @@ let taskBoard = (function () {
                                 this.alert[2].setAttribute('style', 'display:none');
                                 this.alert[2].innerHTML = '';
                         }
-                        //--------END Of date matches dateRegex and Current Year--------//
-
-                        //--------Start of time matches timeRegex--------//
-
                         if (!this.inputTime.match(this.timeRegex)) {
 
                                 this.alert[3].setAttribute('style', 'display:block');
@@ -124,35 +180,11 @@ let taskBoard = (function () {
                                 this.alert[3].setAttribute('style', 'display:none');
                                 this.alert[3].innerHTML = '';
                         }
-                        //--------END Of time matches timeRegex--------//
+                        //--------END Of date matches dateRegex and Current Year--------//
 
-                        this.newNote = new this.Note(this.inputTitle, this.inputTask, this.inputDate, this.inputTime);
+                        this.newNote = new this.NoteConstractor(this.inputTitle, this.inputTask, this.inputDate, this.inputTime);
                         this.saveLocalStorage();
-                        this.renderTemplates();
-                },
-                expiredNote: function () {
-
-                        this.expired = document.getElementsByClassName('expiredDate');
-
-                        for (let i = 0; i < notes.notesArray.length; i++) {
-
-                                const noteDay = notes.notesArray[i].date.split('/')[0];
-                                const noteMonth = notes.notesArray[i].date.split('/')[1];
-                                const noteYear = notes.notesArray[i].date.split('/')[2];
-                                const noteHour = notes.notesArray[i].time.split(':')[0];
-                                const noteMin = notes.notesArray[i].time.split(':')[1];
-                                this.expiredDate = new Date(noteYear, (noteMonth - 1), noteDay, noteHour, noteMin, (new Date().getSeconds()), (new Date().getMilliseconds()));
-                                this.currentDate2 = new Date();
-
-                                if (this.expiredDate < this.currentDate2) {
-
-                                        this.expired[i].setAttribute('style','margin-bottom: -25px;');
-                                        this.expired[i].innerHTML = '* This note has <strong>Expired</strong>';
-                                } else {
-                                        this.expired[i].setAttribute('style','display:none;');
-                                        this.expired[i].innerHTML = '';
-                                }
-                        }
+                        startSite.init();
                 },
                 saveLocalStorage: function () {
 
@@ -168,67 +200,96 @@ let taskBoard = (function () {
                                 notes.notesArray = temp;
                         }
                 },
-                Note: function (title, text, date, time) {
+                expiredNote: function () {
 
-                        this.title = title;
-                        this.text = text;
-                        this.date = date;
-                        this.time = time;
-                        this.justPosted = 'invisi';
+                        this.expired = document.getElementsByClassName('expiredDate');
 
-                        return notes.notesArray.push({ title: this.title, text: this.text, date: this.date, time: this.time, justPosted: this.justPosted });
+                        if (this.expired) {     // On editTask this.expired will be undefined
+
+                                for (let i = 0; i < notes.notesArray.length; i++) {
+
+                                        const noteDay = notes.notesArray[i].date.split('/')[0];
+                                        const noteMonth = notes.notesArray[i].date.split('/')[1];
+                                        const noteYear = notes.notesArray[i].date.split('/')[2];
+                                        const noteHour = notes.notesArray[i].time.split(':')[0];
+                                        const noteMin = notes.notesArray[i].time.split(':')[1];
+                                        this.expiredDate = new Date(noteYear, (noteMonth - 1), noteDay, noteHour, noteMin, (new Date().getSeconds()), (new Date().getMilliseconds()));
+                                        this.currentDate2 = new Date();
+
+                                        if (this.expiredDate < this.currentDate2) {
+
+                                                this.expired[i].setAttribute('style', 'margin-bottom: -25px;');
+                                                this.expired[i].innerHTML = '* This note has <strong>Expired</strong>';
+                                        } else {
+                                                this.expired[i].setAttribute('style', 'display:none;');
+                                                this.expired[i].innerHTML = '';
+                                        }
+                                }
+                        }
 
                 },
-                removeNote: function (id) {
+        }
+
+        let editTask = {
+
+                arr: [],
+
+                noteToEdit: function (id) {
 
                         for (let i = 0; i < notes.notesArray.length; i++) {
-                                if (notes.notesArray[i].title === id) {
 
+                                if (notes.notesArray[i].title + 2 === id) {
+
+                                        editTask.arr.push(notes.notesArray[i]);
                                         notes.notesArray.splice(i, 1);
-                                        document.getElementById(id + 1).setAttribute('style', 'transition:0.4s ease-out; opacity:0;');
-                                        this.expired[i].setAttribute('style', 'transition:0.4s ease-out; opacity:0;');
-                                        
+
                                         setTimeout(() => {
-                                                this.saveLocalStorage();
-                                                this.renderTemplates();
-                                                this.expiredNote();
+                                                this.init();
                                         }, 500);
                                 }
-
                         }
                 },
+                init: function () {
+
+                        this.cacheDomHeader();
+                        this.cacheDomMain();
+                        this.cacheDomFooter();
+                        this.renderEditNote();
+                },
+                cacheDomHeader: function () {
+
+                        this.header = document.getElementById('title');
+                },
+                cacheDomMain: function () {
+
+                        this.main = document.getElementById('newTask');
+                },
+                cacheDomFooter: function () {
+
+                        this.footer = document.getElementById('notes');
+                },
+                renderEditNote: function () {
+
+                        this.header.innerHTML = boardTitleTemplate();
+                        this.main.innerHTML = Mustache.to_html(editTaskTemplate(), editTask.arr[0]);
+                        this.footer.innerHTML = '';
+                },
+                updateTask: function () {
+
+                        this.arr.splice(0, 1);
+
+                        if (startSite.createNote()) {
+
+                                startSite.saveLocalStorage();
+                                startSite.init();
+                        }
+                }
         }
 
-        let notes = {
-
-                notesArray: [{
-                        title: "Example",
-                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie tortor vitae gravida aliquam. Pellentesque ultrices sapien at felis hendrerit vestibulum. In hac habitasse platea dictumst. Suspendisse sit amet lorem id leo varius sodales. Ut sit amet massa ligula. Mauris sit amet tortor nec tellus tincidunt scelerisque. Aliquam eget mauris vel nunc cursus placerat.",
-                        date: '08/09/2018',
-                        time: '11:11',
-                        justPosted: 'visi'
-                },
-                {
-                        title: "Example2",
-                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie tortor vitae gravida aliquam. Pellentesque ultrices sapien at felis hendrerit vestibulum. In hac habitasse platea dictumst. Suspendisse sit amet lorem id leo varius sodales. Ut sit amet massa ligula. Mauris sit amet tortor nec tellus tincidunt scelerisque. Aliquam eget mauris vel nunc cursus placerat.",
-                        date: '08/11/2018',
-                        time: '11:11',
-                        justPosted: 'visi'
-                },
-                {
-                        title: "Example3",
-                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla molestie tortor vitae gravida aliquam. Pellentesque ultrices sapien at felis hendrerit vestibulum. In hac habitasse platea dictumst. Suspendisse sit amet lorem id leo varius sodales. Ut sit amet massa ligula. Mauris sit amet tortor nec tellus tincidunt scelerisque. Aliquam eget mauris vel nunc cursus placerat.",
-                        date: '08/09/2018',
-                        time: '11:11',
-                        justPosted: 'visi'
-                },
-                ],
-        }
         startSite.init();
 
         return {
-                startSite: startSite
+                startSite: startSite,
+                editTask: editTask
         }
-
-
 })();
--- Projects/First Project HTML5/templates.js
+++ Projects/First Project HTML5/templates.js
@@ -11,22 +11,21 @@ function boardTitleTemplate() {
 function createTaskTemplate() {
     return `
     <div class="container">
-    <div class="row title">
-        <div class="col-sm-1"></div>
-        <div class="col-sm-3">
-            <h2 id="headerNewTask">Task Title:</h2>
-        </div>
+        <div class="row title">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-3">
+                <h2 id="headerNewTask">Task Title:</h2>
+            </div>
         <div class="col-sm-7">
-            <input id="titleTask" type="text" class="inputValidation" placeholder="What are you planing to do?" maxlength="15" style="width:230px" required/>
+            <input id="titleTask" type="text" class="inputValidation" placeholder="What are you planing to do?" maxlength="15" style="width:230px" required />
             <p class="alertNotification"></p>
         </div>
     </div>
-</div>
     <div class="row">
         <div class="col-sm-1 newTask-text-center"></div>
         <div class="col-sm-10 newTask-text-padding">
             <p class="alertNotification"></p>
-            <textarea id="textTask" type="text" class="inputValidation" placeholder="Type it here..." rows="3" required></textarea>            
+            <textarea id="textTask" type="text" class="inputValidation" placeholder="Type it here..." rows="3" required></textarea>
         </div>
     </div>
     <div class="container">
@@ -41,19 +40,19 @@ function createTaskTemplate() {
             <div class="col-sm"></div>
             <div class="col-sm-5">
                 <label><strong>Date:</strong>
-                <input id="dateTask" type="text" class="inputValidation" placeholder="dd/mm/yyyy" maxlength="10" style="max-width:100px" required/>
+                <input id="dateTask" type="text" class="inputValidation" placeholder="dd/mm/yyyy" maxlength="10" style="max-width:100px" required />
                 </label>
                 <p class="alertNotification"></p>
             </div>
             <div class="col-sm-5">
                 <label><strong>Time:</strong>
-                <input id="timeTask" class="inputValidation" placeholder="hh:mm" maxlength="11" style="max-width:100px" required/>
+                <input id="timeTask" class="inputValidation" placeholder="hh:mm" maxlength="11" style="max-width:100px" required />
                 </label>
                 <p class="alertNotification"></p>
             </div>
         </div>
     </div>
-    <div  style="width:100%; text-align: center;">
+    <div style="width:100%; text-align: center;">
         <div>
             <button onclick="taskBoard.startSite.createNote()" id="addTaskBtn" class="btn btn-primary">Add Task</button>
         </div>
@@ -63,31 +62,81 @@ function createTaskTemplate() {
 
 function displayNoteTemplate() {
     return `
-        <div class="showExpiredDate">
-            <span class="expiredDate"></span>
-            <div  id="{{title}}1" class="col-sm-2 notes-img note show-remove {{justPosted}}">
-                <div class="row align-items-start">
-                    <div class="col-sm"><span class="underline"><strong>{{title}}</strong></span></div>
-                    <div class="col-sm trash-place">
-                        <i onclick="taskBoard.startSite.removeNote(id)" id="{{title}}" class="far fa-trash-alt remove"></i>
-                    </div>
+    <div class="showExpiredDate">
+        <span class="expiredDate"></span>
+        <div id="{{title}}1" class="col-sm-2 notes-img note show-remove {{justPosted}}">
+            <div class="row align-items-start">
+                <div class="col-sm-6"><span class="underline"><strong>{{title}}</strong></span></div>
+                <div class="col-sm-6 trash-place">
+                    <i onclick="taskBoard.startSite.removeNote(id)" id="{{title}}" class="far fa-trash-alt remove"></i>
+                    <a href="#" data-href="{{title}}" id="{{title}}2" onclick="taskBoard.editTask.noteToEdit(id)"><i class="fas fa-pencil-alt editNote"></i></a>
                 </div>
-                <div class="row align-items-center">                
-                    <div class="col">
+            </div>
+            <div class="row align-items-center">
+                <div class="col">
                     <div id="textNote">{{text}}</div>
-                    </div>
                 </div>
-                <div class="row align-items-end">
-                    <div class="col-8">
-                        <span>{{date}}</span>
-                    </div>
-                    <div class="col-8">
-                        <span>{{time}}</span>
-                    </div>
+            </div>
+            <div class="row align-items-end">
+                <div class="col-8">
+                    <span>{{date}}</span>
                 </div>
-            </div>   
+                <div class="col-8">
+                    <span>{{time}}</span>
+                </div>
+            </div>
         </div>
-
+    </div>
     `
 }
 
+function editTaskTemplate() {
+    return `
+    <div class="container">
+        <div class="row title">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-3">
+                <h2 id="headerNewTask">Task Title:</h2>
+            </div>
+            <div class="col-sm-7">
+                <input id="titleTask" type="text" class="inputValidation" placeholder="What are you planing to do?" value="{{title}}" maxlength="15" style="width:230px" required />
+                <p class="alertNotification"></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-1 newTask-text-center"></div>
+            <div class="col-sm-10 newTask-text-padding">
+                <p class="alertNotification"></p>
+                <textarea id="textTask" type="text" class="inputValidation" placeholder="Type it here..." rows="3" required>{{text}}</textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm"></div>
+            <div class="col-sm-3">
+                <h3 id="headerNewTask">Goal Time:</h3>
+            </div>
+            <div class="col-sm"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm"></div>
+            <div class="col-sm-5">
+                <label><strong>Date:</strong>
+                    <input id="dateTask" type="text" class="inputValidation" placeholder="dd/mm/yyyy" value="{{date}}" maxlength="10" style="max-width:100px" required />
+                </label>
+                <p class="alertNotification"></p>
+            </div>
+            <div class="col-sm-5">
+                <label><strong>Time:</strong>
+                    <input id="timeTask" class="inputValidation" placeholder="hh:mm" value="{{time}}" maxlength="11" style="max-width:100px" required />
+                </label>
+                <p class="alertNotification"></p>
+            </div>
+        </div>
+    </div>
+    <div style="width:100%; text-align: center;">
+        <div>
+            <button onclick="taskBoard.editTask.updateTask()" id="addTaskBtn" class="btn btn-primary">Edit Task</button>
+        </div>
+    </div>
+    `
+}
